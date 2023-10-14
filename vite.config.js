bc0fbd18e2d018645987fe836c52ed165a0b631f
dilan besong <besongdilan@gmail.com>
2023-10-14 11:54:13 +0100

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://cee-info.onrender.com" || "http://localhost:5000",
+        target: "https://cee-info.onrender.com/" || "http://localhost:5000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace("/api", ""),
