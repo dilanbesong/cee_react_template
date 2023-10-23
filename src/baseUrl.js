801157f82544269c17fd70bc5e977a539940fd58
dilan besong <besongdilan@gmail.com>
2023-10-23 21:00:50 +0100

    updated baseUrl

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,3 +1,4 @@
 // http://localhost:5000
 //https://ceebackend-production.up.railway.app
-export const BASEURL = "https://ceebackend-production.up.railway.app";
+//https://cee-departmental-app-backend.onrender.com
+export const BASEURL = "https://cee-departmental-app-backend.onrender.com";
