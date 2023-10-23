389922e1aadf8aafa6e97753c089b5bd558bd3e2
dilan besong <besongdilan@gmail.com>
2023-10-23 20:54:59 +0100

    updated baseUrl

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,3 +1,3 @@
 // http://localhost:5000
-//
-export const BASEURL = "https://cee-departmental-app-backend.onrender.com";
+//https://ceebackend-production.up.railway.app
+export const BASEURL = "https://ceebackend-production.up.railway.app";
