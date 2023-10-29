f706e786ae5798113a1fd8a595f698303a973327
dilan besong <besongdilan@gmail.com>
2023-10-29 22:29:24 +0100

    fixed page navigation

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -2,4 +2,5 @@
 //https://ceebackend-production.up.railway.app
 //https://cee-departmental-app-backend.onrender.com
 //import.meta.env.VITE_BASEURL
+
 export const BASEURL = import.meta.env.VITE_BASEURL;
