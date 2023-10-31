e42f9b5429a1892f5e7c3d88457d5e1dc19e4bc8
dilan besong <besongdilan@gmail.com>
2023-10-31 11:02:39 +0100

    fix createGroup route

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -2,4 +2,4 @@
 //https://ceebackend-production.up.railway.app
 //import.meta.env.VITE_BASEURL
 
-export const BASEURL = 'http://localhost:5000'
+export const BASEURL = import.meta.env.VITE_BASEURL;
